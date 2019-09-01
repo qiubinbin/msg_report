e92a44863142221c62ae058eda5f188f9d4076c2
qiubinbin <qiubin780061109@163.com>
2019-09-01 20:56:54 +0800

    20190901启动界面 BMP RGB 150PPI

--- startup.py
+++ startup.py
@@ -1,10 +1,14 @@
 import sys
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 from main_win import Msg_als
 
 app = QtWidgets.QApplication(sys.argv)
+splash = QtWidgets.QSplashScreen(QtGui.QPixmap('icon/ml.bmp'))
+splash.show()
+QtWidgets.qApp.processEvents()
 win = Msg_als()
 win.show()
+splash.close()
 sys.exit(app.exec_())
